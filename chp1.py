e7dec1fa2b8df71b08cb9e021a8bf36948735afc
Vanshita613 <kalyanevanshita07@gmail.com>
2026-05-24 15:36:39 +0530

    variable

--- chp1.py
+++ chp1.py
@@ -1,1 +1,45 @@
-print(("python "+ "\n") *7)
+# print(("python "+ "\n") *7)
+
+# name = "sia"
+# print("may name is: ", name)
+
+#input---
+
+# name= input("enter your name:")
+# print("the user is : ", name)
+
+
+# a=7
+# b=8
+# sum=a + b
+# print(sum) 
+
+
+# name = input("enter your name: ")
+# age = input("age:")
+# city=input("enter your city:")
+
+# print("name:" , name)
+# print("age :" ,age)
+# print("city :", city)
+
+# Q1 Write a Program to input 2 numbers & print their sum.
+
+# a = int(input("enter the first number :"))
+# b =int( input("enter the second number :"))
+
+# print("the sum is: ", a+b)
+
+# Q2 WAP to input side of a square & print its area.
+
+# side=int(input("enter the side:"))
+# area=(side*side)
+# print("the area of square is: ", area)
+
+# Q3 WAP to input 2 floating point numbers & print their average.
+a = float(input("enter the first value:"))
+b = float(input("enter the second value:"))
+
+avg= ((a+b)/2)
+
+print("the average of the numb is =", avg)

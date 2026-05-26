ca5e6778208d96e3d745cdae4a0bddba49760412
Vanshita613 <kalyanevanshita07@gmail.com>
2026-05-26 21:05:25 +0530

    dict

--- dict.py
+++ dict.py
@@ -1,7 +1,44 @@
-info = {
-    "name" : "sia",
-    "age" : 17,
-    "city":"bombay",
-}
+# info = {
+#     "name" : "sia",
+#     "age" : 17,
+#     "city":"bombay",
+# }
 
-print(info.items())
+# print(info.items())
+
+# # Q . Store following word meanings in a python dictionary :
+# table: "a piece of furniture", "list of facts & figures"
+# cat: "a small animal"
+
+# dict={
+#     "table" :[ "a piece of furniture","a list of facts"],
+#     "cat" : "a small animal"
+# }
+
+# print ( dict)
+
+
+# subj={
+#     "python", "javascript" ,"c","python", "javascript","c++", "javascript"
+# }
+
+# print(len(subj))
+
+
+# WAP to enter marks of 3 subjects from the user and store them in a dictionary. Start with an empty dictionary & add one by one. Use subject name as key & marks as value.
+
+# marks={}
+
+# a=int(input("enter marks of physics:"))
+# marks.update({"physics":a})
+
+# b=int(input("enter marks of chemistry:"))
+# marks.update({"chemistry":b})
+
+# c=int(input("enter marks of maths:"))
+# marks.update({"maths":c})
+
+# print(marks)
+
+
+ 

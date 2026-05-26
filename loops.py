ebd9d956ebe4fe1c6523d4c1b0a75b6742710250
Vanshita613 <kalyanevanshita07@gmail.com>
2026-05-26 21:21:16 +0530

    while loop

--- loops.py
+++ loops.py
@@ -1,4 +1,16 @@
-count=1
-while count <=7 :
-    print("python")
-    count+=1
+# count=1
+# while count <=7 :
+#     print("python")
+#     count+=1
+
+# Q print numbers from 1 to 100.
+# i=1
+# while i<=100:
+#     print (i)
+#     i+=1
+
+# Q print numbers from 100 to 1
+# i=100
+# while i >=1:
+#   print (i)
+#   i-=1

22277c459ec5b6dde4d22d6dd8e4e45ad8ed902c
Vanshita613 <kalyanevanshita07@gmail.com>
2026-05-29 21:24:44 +0530

    tuple

--- loops.py
+++ loops.py
@@ -14,3 +14,24 @@
 # while i >=1:
 #   print (i)
 #   i-=1
+
+# Q. print the multipliction table of a nubmer n
+# n=int(input("enter a number:"))
+# i=1
+# while i <=10:
+#     print (i*n)
+#     i+=1
+
+
+# index=0
+# nums=[1 , 4, 9, 16,25, 36, 49, 64, 81,100]
+# while index< len(nums):
+#     print(nums[index])
+#     index+=1
+
+
+nums=[1 , 4, 9, 16,25, 36, 49, 64, 81,100]
+index=0
+i=25
+while index< len(nums):
+    print(i)

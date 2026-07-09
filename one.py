2bbcf103b86ba62aeeeaf1a367ad73fdaa7bbbae
Vanshita613 <kalyanevanshita07@gmail.com>
2026-07-09 20:21:09 +0530

    .

--- one.py
+++ one.py
@@ -228,3 +228,14 @@
 # def calc_fact(n):
 #     fact=1
 #
+
+n=int(input("enter a number:"))
+
+if ( n %2 !=0):
+    print("Weird")
+if (n%2==0 and 2<= n<=5):
+    print("Not Weird")
+if (n%2==0 and 6<=n<=20):
+    print("Weird")
+if ( n%2==0 and n>20):
+    print("Not Weird")

26bec6ef8bb99e80e106f8851a09f15dc68004ce
Vanshita613 <kalyanevanshita07@gmail.com>
2026-07-15 17:32:52 +0530

    .

--- funct.py
+++ funct.py
@@ -1,14 +1,26 @@
-def my_function():
-  print("Hello from a function")
+# def my_function():
+#   print("Hello from a function")
 
-my_function()
-my_function()
-my_function()
+# my_function()
+# my_function()
+# my_function()
 
-# temp conversion
-def fahrenheit_to_celsius(fahrenheit):
-  return (fahrenheit - 32) * 5 / 9
+# # temp conversion
+# def fahrenheit_to_celsius(fahrenheit):
+#   return (fahrenheit - 32) * 5 / 9
 
-print(fahrenheit_to_celsius(77))
-print(fahrenheit_to_celsius(95))
-print(fahrenheit_to_celsius(50))
+# print(fahrenheit_to_celsius(77))
+# print(fahrenheit_to_celsius(95))
+# print(fahrenheit_to_celsius(50))
+
+
+# print sum
+# def sum(a,b):
+#     return (a+b)
+# print(sum(4,5))
+
+
+# print difference
+def diff(a,b):
+    return(a-b)
+print(diff(9,8))

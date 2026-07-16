645c5ec8e4596de63ddddf144126f68e0ccfadcb
Vanshita613 <kalyanevanshita07@gmail.com>
2026-07-16 20:54:08 +0530

    .

--- list.py
+++ list.py
@@ -1,9 +1,22 @@
 
-list=[2,4,5]
-list.append(9)
-print(list) 
+# list=[2,4,5]
+# list.append(9)
+# print(list) 
 
 
-list=[2,4,5]
-list.append(21)
-print(list)
+# list=[2,4,5]
+# list.append(21)
+# print(list)
+
+
+#  WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+# movies=[]
+# mov1=input("enter 1st movie:")
+# mov2=input("enter 2nd movie:")
+# mov3=input("enter 3rd movie:")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)

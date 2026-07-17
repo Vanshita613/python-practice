63b03042977f5a7c241f2be68501f6754c4479fd
Vanshita613 <kalyanevanshita07@gmail.com>
2026-07-17 21:07:57 +0530

    .

--- list.py
+++ list.py
@@ -28,3 +28,12 @@
 
 # list=["sia","rohan","tia","varun","sam"]
 # print(list, end=" ")
+
+# factorial of a number
+n=int(input("enter a number:"))
+fact=1
+i=1
+while i<=n:
+    fact=fact*i
+    i=i+1
+print(fact)

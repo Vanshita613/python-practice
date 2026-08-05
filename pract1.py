e90474871d44e164202757c3d4f05abd1dd23e11
Vanshita613 <kalyanevanshita07@gmail.com>
2026-08-05 20:24:19 +0530

    .

--- pract1.py
+++ pract1.py
@@ -8,3 +8,29 @@ while i<=num:
     fact=fact*i
     i+=1
 print(fact)
+
+#check prime number 
+num=int(input("enter a number:"))
+
+if num<=1:
+    print(num,"is not a prime number!")
+else:
+    for i in range(2,num):
+        if num%i==0:
+            print(num,"is not a prime number")
+            break
+    else:
+            print(num,"is a prime number!")
+
+
+#fibonacci series
+n=int(input("enter a number:"))
+
+a=0
+b=1
+
+for i in range(n):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c

c695e11301ad6ca265ad5edfad477cac86d2bd5f
Vanshita613 <kalyanevanshita07@gmail.com>
2026-08-21 22:39:54 +0530

    .

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write("hello world")
+
+st.write("This is a simple Streamlit app that demonstrates how to use Streamlit for building web applications. You can add more features and functionalities as needed.")
+
+st.markdown("## this is large heading")
+
+st.markdown("# this is small heading")

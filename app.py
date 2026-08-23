ea8c5da1801c25b94db56fc1d0005026a035b484
Vanshita613 <kalyanevanshita07@gmail.com>
2026-08-23 19:11:49 +0530

    .

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 st.write("hello world")  #display text
 
+
+
 st.write("This is a simple Streamlit app that demonstrates how to use Streamlit for building web applications. You can add more features and functionalities as needed.")
+
+
 
 st.markdown("## this is large heading")   #large heading
 
@@ -20,5 +24,7 @@ st.image("streamlit_hero.jpg" )
 
 
 bias=st.selectbox("select your bias:", ["jay","niki", "arisu","huta","keeho","intak"])
+st.write(f"you selected: {bias}")
+st.success("damnn u have great bias!")
 
 
